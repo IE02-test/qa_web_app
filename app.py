18657d9a3890fc97d3007cbb3834faef47f9cadb
IE02-test <shinodaitv@gmail.com>
2024-09-11 10:59:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__, template_folder='templates')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///qa.db'
